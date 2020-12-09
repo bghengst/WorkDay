b0c05a6d8ba1fb044ed85792358013c903c3ffe8
Grant <beauhengst@gmail.com>
2020-12-09 06:57:28 -0500

    added code in script.js to let program know when a time slot is in past

--- script.js
+++ script.js
@@ -17,7 +17,15 @@ $(document).ready(function(){
 
         $(".time-block").each(function(){
             var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-            
+
+            if (blockTime < timeNow){
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+                $(this).addClass("past");
+            }
+
+
+
         })
 
 
