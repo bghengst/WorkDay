0a266d1a09f1f0ea9ad1d15ef99fb4c9a1228d9d
Grant <beauhengst@gmail.com>
2020-12-09 06:41:16 -0500

    created function in js to save the text that was entered to each corresponding time to save in each time slots local storage

---
+++ script.js
@@ -0,0 +1,16 @@
+var todayDate = moment().format('dd, MM YYYY');
+$("#currentDay").text(todayDate)
+
+$(document).ready(function(){
+
+    $(".saveBTN").on("click", function()){
+
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time,text);
+    }
+
+    
+
+})
